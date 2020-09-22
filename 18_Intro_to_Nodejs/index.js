85a7ecf53061db7c5a0f2ebc5c4178713c3402f4
Thomas Chu <thomasuhc@gmail.com>
2020-09-22 17:41:15 -0400

    Final Chapter 18
    End exercises Nodejs and NPM packages

---
+++ 18_Intro_to_Nodejs/index.js
@@ -0,0 +1,16 @@
+const superheroes = require('superheroes');
+ 
+ 
+var mySuperHeroName = superheroes.random();
+//=> 'Spider-Ham'
+
+console.log(mySuperHeroName);
+
+
+const supervillains = require('supervillains');
+
+ 
+var mySuperVillainsName = supervillains.random();
+//=> 'Mud Pack'
+
+console.log(mySuperVillainsName);
