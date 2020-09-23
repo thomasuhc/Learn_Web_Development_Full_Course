789bc6ef5d801155587ff83a29d5ef2109e40e07
Thomas Chu <thomasuhc@gmail.com>
2020-09-23 16:27:22 -0400

    Finish send Url
    Using Express to Render a Website with Live API Data

--- 20_Weather_Project/app.js
+++ 20_Weather_Project/app.js
@@ -6,7 +6,35 @@ const https = require('https');
 const app = express();
 
 app.get('/', function (req, res) {
-    res.send("<h1>World hello again</h1>");
+
+
+    const url = "https://api.openweathermap.org/data/2.5/weather?q=toronto&units=metric&appid=d2893f0cdfb00aa69eb343a3c0f11c24";
+
+    https.get(url, function(response) {
+        console.log(response.statusCode);
+
+        response.on("data" , function(data) {
+            const weatherData = JSON.parse(data);
+
+            const temp = weatherData.main.temp;
+            const weatherDescription = weatherData.weather[0].description;
+            const icon = weatherData.weather[0].icon;
+
+            const imageUrl = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+
+
+            res.write("<p>The weather is currently " + weatherDescription + "</p>");
+
+            res.write("<h1>The temperature in Toronto is " + temp + " Degrees Celsius</h1>");
+
+            res.write("<img src=" + imageUrl + ">");
+
+            res.send();
+            
+        });
+    
+    });
+
 });
 
 
