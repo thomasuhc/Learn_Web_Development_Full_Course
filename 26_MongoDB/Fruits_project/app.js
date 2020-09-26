7c88eddb1d0ac2b5c078733bb414d748889eb424
Thomas Chu <thomasuhc@gmail.com>
2020-09-26 18:08:18 -0400

    Completed Chapter 26
    MongoDB completed exercise

--- 26_MongoDB/Fruits_project/app.js
+++ 26_MongoDB/Fruits_project/app.js
@@ -1,23 +1,66 @@
-const { MongoClient } = require("mongodb");
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 
-// Connection URI
-const uri =
-  "mongodb+srv://sample-hostname:27017/?poolSize=20&w=majority";
+// Connection URL
+const url = 'mongodb://localhost:27017';
+
+// Database Name
+const dbName = 'fruitsDB';
 
 // Create a new MongoClient
-const client = new MongoClient(uri);
+const client = new MongoClient(url);
 
-async function run() {
-  try {
-    // Connect the client to the server
-    await client.connect();
+// Use connect method to connect to the Server
+client.connect(function(err) {
+  assert.strictEqual(null, err);
+  console.log("Connected successfully to server");
 
-    // Establish and verify connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Connected successfully to server");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    await client.close();
-  }
+  const db = client.db(dbName);
+
+  findDocuments(db, function() {
+    client.close();
+  });
+});
+
+
+const insertDocuments = function(db, callback) {
+
+    const collection = db.collection('fruits');
+
+    collection.insertMany ([
+        {
+            name: "Apple",
+            score: 10,
+            review: "Great fruit"
+        }, 
+        {
+            name: "Orange",
+            score: 7,
+            review: "More or less"
+        }, 
+        {
+            name: "Banana",
+            score: 5,
+            review: "I dont like"
+        }
+
+    ], function(err, result) {
+        assert.strictEqual(err, null);
+        assert.strictEqual(3, result.result.n);
+        assert.strictEqual(3, result.ops.length);
+        console.log("Insert 3 document into the collections");
+        callback(result);
+    });
 }
-run().catch(console.dir);
+
+const findDocuments = function(db, callback) {
+
+    const collection = db.collection('fruits');
+
+    collection.find({}).toArray(function(err, fruits) {
+        assert.strictEqual(err, null);
+        console.log("Found the following records");
+        console.log(fruits);
+        callback(fruits);
+    });
+}
