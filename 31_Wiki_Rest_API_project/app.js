311626bd606c81e7b822a12849e63c7eadc80760
Thomas Chu <thomasuhc@gmail.com>
2020-09-28 20:33:12 -0400

    Step 1 completed
    Step 1 get completed

--- 31_Wiki_Rest_API_project/app.js
+++ 31_Wiki_Rest_API_project/app.js
@@ -25,8 +25,17 @@ const articleSchema = {
    const Article = mongoose.model("Article", articleSchema);
 
 
+app.get("/articles", function(req, res){
 
+    Article.find(function(err, foundArticles){
 
+        if(!err) {
+            res.send(foundArticles);
+        } else {
+            res.send(err);
+        }
+    })
+});
 
 
 app.listen(3000, function() {
