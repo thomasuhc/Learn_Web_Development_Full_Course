5f5b526aab82805249a766542edb9850c1eeea61
Thomas Chu <thomasuhc@gmail.com>
2020-09-29 14:26:14 -0400

    Step 1 Basic code
    Basic code

--- 32_Authentication_Security/app.js
+++ 32_Authentication_Security/app.js
@@ -1,1 +1,36 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require('mongoose');
 
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+//////////////////////////////////////////////////////////
+
+app.get("/", function(req, res){
+    res.send("home");
+});
+
+app.get("/", function(req, res){
+    res.send("home");
+});
+
+
+
+
+
+
+
+
+//////////////////////////////////////////////////////////
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+  });
+  
+  
