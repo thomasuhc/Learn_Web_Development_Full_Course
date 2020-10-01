64b24fdd53ef9560635d1b5a6469c715827af063
Thomas Chu <thomasuhc@gmail.com>
2020-10-01 18:50:38 -0400

    Comp folder in wrong place
    Move folder to the right place

--- 33_Reactjs/keeper_app_project/src/components/App.jsx
+++ 33_Reactjs/keeper_app_project/src/components/App.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
 function App() {
-    return 
+    return (
     <div>
-    <h1>Hello App</h1>
+        <h1>Hello App</h1>
     </div>
+    );
 }
 
 export default App;
--- 33_Reactjs/keeper_app_project/src/index.js
+++ 33_Reactjs/keeper_app_project/src/index.js
@@ -1,16 +1,16 @@
-//1. Create a new React app.
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from "./components/App";
 
-import React from "react";
-import ReactDOM from "react-dom";
 
 ReactDOM.render(
-    <h1>
-      Hello again react !!!!!!!
-    </h1>,
-    document.getElementById('root')
-  );
-  
+  < App />,
+  document.getElementById('root')
+);
 
+
+
+//1. Create a new React app.
 //2. Create a App.jsx component.
 //3. Create a Header.jsx component that renders a <header> element
 //to show the Keeper App name in an <h1>.
